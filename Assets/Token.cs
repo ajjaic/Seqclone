865c4720a79d70c8d71808d615003baf8e610144
AJ <ajjaic@cseries.us>
2020-04-17 17:06:07 -0400

    created grid, tokens. restrict token movement to within square.

---
+++ Assets/Token.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utils;
+
+[ExecuteInEditMode]
+public class Token : MonoBehaviour
+{
+    private void Update()
+    {
+        float xPos = Grids2DSquare.GetGridPosClamp(transform.position.x, 10, 5, 5, 95);
+        float zPos = Grids2DSquare.GetGridPosClamp(transform.position.z, 10, 5, 5, 95);
+        transform.position = new Vector3(xPos, transform.position.y, zPos);
+    }
+}
---
+++ Assets/Utils/Grids2D.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Utils
+{
+    public static class Grids2DSquare 
+    {
+        public static float GetGridPos(float currentAxisPos, int gridSize, int offset)
+        {
+            float newAxisPos = (Mathf.Floor(currentAxisPos / gridSize) * gridSize) + 5;
+            return newAxisPos;
+        }
+
+        public static float GetGridPosClamp(float currentAxisPos, int gridSize, int offset, int currentAxisMin, int currentAxisMax)
+        {
+            float newAxisPos = GetGridPos(currentAxisPos, gridSize, offset);
+            float clampedNewAxisPos = Mathf.Clamp(newAxisPos, currentAxisMin, currentAxisMax);
+            return clampedNewAxisPos;
+        }
+    }
+}
